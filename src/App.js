4f9003def8f45c07893784da8339603103a86b90
Tanim <Tanim.i343@gmail.com>
2023-05-28 22:34:02 -0400

    sliders (carousel)centered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { query } from './imageAPI';
 import './App.css';
 
