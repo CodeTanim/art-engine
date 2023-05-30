5bace8bbeb93050e691cf7cf4be3e37bc82a1c47
Tanim <Tanim.i343@gmail.com>
2023-05-30 18:33:15 -0400

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { query } from './imageAPI';
 import './App.css';
 
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/example-prompts">Example Prompts</a></li>
+      </ul>
+    </nav>
+  );
+}
+
 function App() {
   const [seedText, setSeedText] = useState('');
   const [imageSrcs, setImageSrcs] = useState([]);
@@ -16,9 +27,10 @@ function App() {
   const generateImages = async () => {
     setIsLoading(true);
     const newImageSrcs = [];
+    const mid = 'mdjrny-v4 style';
     for (let i = 0; i < 4; i++) {
       const randomNumber = Math.floor(Math.random() * 1000000);
-      const response = await query({inputs: `${seedText} ${randomNumber}`});
+      const response = await query({inputs: `${seedText} ${randomNumber} ${mid}`});
       newImageSrcs.push(URL.createObjectURL(response));
     }
     setImageSrcs(newImageSrcs);
@@ -27,6 +39,7 @@ function App() {
 
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <div className="input-group">
           <input type="text" value={seedText} onChange={handleInputChange} placeholder="Enter prompt"/>
